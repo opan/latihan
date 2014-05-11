75ce1ffaf41ca5a7fd08f49bce95d1410557c0b8
opan mustopah <opan.nneutron>
2014-05-11 19:54:55 +0700

    fisrt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Latihan::Application.config.secret_key_base = '2ebbdc783dd76583aaa0280c90065961efa559786628bbf331efdf609ef817eeed7f7d8d2dd6d569af6f68b6422a4809ea29aa70b53797fda55fb2d57dc84ced'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Latihan::Application.config.session_store :cookie_store, key: '_latihan_session'
